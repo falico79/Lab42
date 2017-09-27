99ff23099099b08bf9394d70fea3c6f0c31a1931
falico79 <lasse.siniluoto@gmail.com>
2017-09-27 22:02:31 +0300

    Added chancePrice and test

--- main.java
+++ main.java
@@ -3,6 +3,10 @@ public class main {
     	Product product = new Product("Edam", 3.3, 120);
     	System.out.println("Product value is " + product.countValue());
     	product.printProduct();
+		
+		product.changePrice(4.1);
+		System.out.println("\nNew product value is " + product.countValue());
+    	product.printProduct();
      }
 }
 
@@ -21,5 +25,10 @@ public class main {
  
 	public void printProduct() {
 		System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
+	}
+	
+	public void changePrice(double newPrice) {
+		this.price = newPrice;
+		
 	}
 }
